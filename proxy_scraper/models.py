2cb0840ecd7c18d6cc30a1c7143206148b35b76c
vikas <vikaspatel143735@gmail.com>
2024-02-04 19:27:46 +0530

    set modal created

--- proxy_scraper/models.py
+++ proxy_scraper/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Proxy(models.Model):
+    ip = models.CharField(max_length=15)
+    port = models.CharField(max_length=5)
+    protocol = models.CharField(max_length=10)
+    country = models.CharField(max_length=50)
+    uptime = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"{self.ip}:{self.port} - {self.protocol}"
